30fd41d8095fd794811838b40289de45015bd2ec
Uldis Barbans <84229+ulzha@users.noreply.github.com>
2023-04-18 19:12:03 +0200

    Less Backstage stajls

--- app/src/main/resources/static/spive.js
+++ app/src/main/resources/static/spive.js
@@ -8,10 +8,33 @@ window.onload = function () {
         .on('mouseout', (event, d) => legend
             .style('visibility', 'hidden'));
 
+    d3.select('.content')
+        .on('click', (event, d) => {
+            shite = [
+                {id: 1, name: "App 1"},
+                {id: 2, name: "App 2"},
+                {id: 3, name: "SeriouslyNonfunnyApp"}
+            ];
+
+            for (var app of shite) {
+                var rowHtml = '<td class="MuiTableCell-root-2339 MuiTableCell-body-2341"><a class="text" href="https://company.net/event-sourced-applications/' + app.name + '">' + app.name + '</a></td><td class="MuiTableCell-root-2339 MuiTableCell-body-2341" id="timeline-' + app.id + '"></td>';
+                d3.select('#applications').append('tr').attr('class', 'MuiTableRow-root-2335').html(rowHtml);
+                render_timeline(d3.select('#timeline-' + app.id), 1, 0, '#1db855');
+                let eventHtml = render_event_html({time: new Date(), json: '{"foo": "bar"}', handled_in: '3 ms', status: 'ok', logs_display: 'visible'});
+                d3.select('#applications')
+                  .append('tr')
+                  .append('td').attr('colspan', '2')
+                  .append('table')
+                  .append('tbody')
+                  .append('tr').attr('class', 'event')
+                  .html(eventHtml);
+            }
+        });
+
     d3.json('/api/applications')
         .then(function (apps) {
             for (var app of apps) {
-                var rowHtml = '<td class="MuiTableCell-root-2339 MuiTableCell-body-2341"><a class=" stajl2352" href="https://company.net/event-sourced-applications/' + app.name + '">' + app.name + '</a></td><td class="MuiTableCell-root-2339 MuiTableCell-body-2341" id="timeline-' + app.id + '"></td>';
+                var rowHtml = '<td class="MuiTableCell-root-2339 MuiTableCell-body-2341"><a class="text" href="https://company.net/event-sourced-applications/' + app.name + '">' + app.name + '</a></td><td class="MuiTableCell-root-2339 MuiTableCell-body-2341" id="timeline-' + app.id + '"></td>';
                 d3.select('#applications').append('tr').attr('class', 'MuiTableRow-root-2335').html(rowHtml);
                 render_timeline(d3.select('#timeline-' + app.id), 1, 0, '#1db855');
             }
