2690c60ae92b1dfe43a3beda491a6c3f5b75ecc1
Uldis Barbans <84229+ulzha@users.noreply.github.com>
2024-10-12 16:52:32 +0200

    WIP doesn't work, not a ReactNode

--- app/src/main/qwik/src/components/nav/common.tsx
+++ app/src/main/qwik/src/components/nav/common.tsx
@@ -2,6 +2,10 @@
 
 import { Card } from "@mui/material";
 import HelpButton from "~/components/nav/support/help";
+import ImgArmeria from "~/media/icons/armeria.svg";
+import ImgD3 from "~/media/icons/d3.svg";
+import ImgGitHub from "~/media/icons/github.svg";
+import ImgQwik from "~/media/icons/qwik.svg";
 import { qwikify$ } from "@builder.io/qwik-react";
 import styles from "./nav.module.css";
 
@@ -9,7 +13,7 @@ export default qwikify$((props) => (
   <div className="padding">
     <div className={styles.credits}>
       <a href="https://github.com/ulzha/spive" title="GitHub" target="_blank">
-        <img src="/icons/github.svg" />
+        <ImgGitHub />
       </a>
     </div>
     <div className="titlebar">
@@ -24,13 +28,13 @@ export default qwikify$((props) => (
     <div className={styles.credits}>
       Built with:
       <a href="https://armeria.dev/" title="Armeria" target="_blank">
-        <img src="/icons/armeria.svg" />
+        <ImgArmeria />
       </a>
       <a href="https://qwik.builder.io/" title="Qwik" target="_blank">
-        <img src="/icons/qwik.svg" />
+        <ImgQwik />
       </a>
       <a href="https://d3js.org/" title="D3" target="_blank">
-        <img src="/icons/d3.svg" />
+        <ImgD3 />
       </a>
     </div>
   </div>
--- app/src/main/qwik/src/typings.d.ts
+++ app/src/main/qwik/src/typings.d.ts
@@ -1,1 +1,12 @@
 declare module "*.module.css";
+
+// https://github.com/parcel-bundler/parcel/discussions/7910#discussioncomment-2958845
+declare module "*.svg" {
+  import * as React from "react";
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  export default ReactComponent;
+}
